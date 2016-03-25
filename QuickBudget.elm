9afdc6d7060891571458a8c4e01705dace6c036c
Gage Peterson <justgage@gmail.com>
2016-03-24 23:10:29 -0600

    Got basic undos working.

--- QuickBudget.elm
+++ QuickBudget.elm
@@ -2,12 +2,14 @@ module QuickBudget where
 
 import Html exposing (h3, div, button, text)
 import Html.Events exposing (onClick)
-import Html.Attributes exposing (class)
+import Html.Attributes exposing (class, style)
 
 type alias Budget =
   { old : List Expense
   , new : List Expense
   }
+
+type alias Categories = List String
 
 type alias Expense =
   { name : String
@@ -15,53 +17,131 @@ type alias Expense =
   , cat : String
   }
 
-model : Budget
-model =
-  { old = []
-  , new = [
-       { name = "Walmart"
-       , amount = 12.00
-       , cat = "none"
-       }
-      , { name = "Winco"
-        , amount = 18.00
-        , cat = "none"
-        }
-     ]
+type alias Model =
+  { budget : Budget
+  , cats : Categories
   }
 
-viewExpense : Expense -> Html.Html
-viewExpense x =
+model : Model
+model =
+  {
+    cats = [ "Home"
+           , "Groceries"
+           , "Gages Allowance"
+           ]
+  , budget =
+      { old = []
+      , new =
+          [ { name = "Walmart"
+            , amount = 120.00
+            , cat = "none"
+            }
+          , { name = "Petco"
+            , amount = 8.00
+            , cat = "none"
+            }
+          , { name = "Winco"
+            , amount = 180.00
+            , cat = "none"
+            }
+          , { name = "Zurkers"
+            , amount = 8.00
+            , cat = "none"
+            }
+          , { name = "Humble Bundle"
+            , amount = 1.00
+            , cat = "none"
+            }
+          ]
+      }
+  }
+
+viewCategory : String -> Float -> Html.Html
+viewCategory name total =
+  div [] [h3 [] [text name]
+         , text ("Total: " ++ (toString total))
+         ]
+
+viewExpense : Html.Attribute -> Expense -> Html.Html
+viewExpense sty x =
   div [class "card"]
-        [ h3  [] [text (x.name)]
+        [ h3  [sty] [text (x.name)]
         , div []         [text (toString x.amount)]
         , div []         [text (x.cat)]
         ]
 
-view : Signal.Address Action -> Budget -> Html.Html
+
+viewExpenseNew : Expense -> Html.Html
+viewExpenseNew = viewExpense ( style [("color", "red")] )
+                 
+viewExpenseOld : Expense -> Html.Html
+viewExpenseOld = viewExpense ( style [("color", "black")] )
+
+viewCatButton address b =
+  button
+    [ onClick address (Categorize b)]
+    [ text b ]
+
+viewUndoButton address =
+  button
+    [ onClick address Undo]
+    [ text "Undo" ]
+
+
+view : Signal.Address Action -> Model -> Html.Html
 view address model =
+  let {budget, cats} = model in
+  let viewButton = viewCatButton address in
   div []
-        [ div [] (List.map viewExpense model.new)
-        , button [ onClick address (Categorize "cat")] [ text "Home" ]
+        [ div
+            [] (List.map viewExpenseOld budget.old)
+        , div
+          [] (List.map viewExpenseNew budget.new |> List.reverse)
+        , div
+            [] (viewUndoButton address :: List.map viewButton cats)
         ]
 
 
-type Action = Categorize String--| NoOp
+type Action = Categorize String
+            | Undo
+            | NoOp
 
-update : Action -> Budget -> Budget
+update : Action -> Model -> Model
 update action model =
+  let _ = Debug.watch "action" action in
+  let _ = Debug.watch "model." (toString model) in
   case action of
-    -- NoOp ->
-    --   model
+    NoOp ->
+      model
+
+    Undo ->
+      let {budget} = model in
+      let {old , new} = budget in
+      case old of
+        [] ->
+          model
+
+        last :: old_rest ->
+          { model |
+              budget =
+              { new = last :: new
+              , old = old_rest
+              }
+          }
 
     Categorize cat ->
-      let {old , new} = model in
+      let {budget} = model in
+      let {old , new} = budget in
       case new of
         [] ->
           model
 
         next::rest ->
-          { new = rest
-          , old = next :: old
-          }
+          let next_cat = {next | cat = cat} in
 
+          { model |
+              budget =
+              { new = rest
+              , old = next_cat :: old
+              }
+          }
